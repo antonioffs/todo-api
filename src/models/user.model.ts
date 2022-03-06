5de6d3cdf302cda71b2b8dd1570d869c33b9007d
Antonio Silva <antonio.ffsmail@gmail.com>
2022-03-06 18:35:53 -0300

    WIP: Installed validation packege, but POST is failuring

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,15 +1,24 @@
+import { IsNotEmpty, IsString } from "class-validator";
 import { Column, DataType, HasMany, Model, Table } from "sequelize-typescript";
 import { Todo } from "./todo.model";
 
 @Table
 export class User extends Model<User>{
 
+    @IsNotEmpty({
+        message: "Name must be informed"
+    })
+    @IsString()
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
     name: string;
 
+    @IsNotEmpty({
+        message: "Username must be informed"
+    })
+    @IsString()
     @Column({
         type: DataType.STRING,
         allowNull: false,
