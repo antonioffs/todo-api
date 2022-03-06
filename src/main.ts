1d44387a5cf86b731d1c700c18008baf4f119087
Antonio Silva <antonio.ffsmail@gmail.com>
2022-03-06 20:16:29 -0300

    WIP, instaled Swagger packages

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -7,6 +8,16 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     transform: false,
   }))
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
