eaa2997ecbb52638574140c4ef522493e9fc3afd
Antonio Silva <antonio.ffsmail@gmail.com>
2022-03-06 22:12:51 -0300

    Ajusts on main and added parametes to nest-cli

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({
     transform: false,
-  }))
+    }))
 
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
+    const config = new DocumentBuilder()
+    .setTitle('To Do API')
+    .setDescription('To Do API developed in NestJS')
+    .setVersion('0.0.1')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  
-  await app.listen(3000);
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(3000);
 }
 bootstrap();
